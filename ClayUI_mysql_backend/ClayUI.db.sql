db899553758745bd602eb8f9d44e86f48bedcd9b
Andy Lize <alize@netinfocentral.com>
2012-03-19 23:22:09 -0500

    Added routine for App Details

--- ClayUI_mysql_backend/ClayUI.db.sql
+++ ClayUI_mysql_backend/ClayUI.db.sql
@@ -181,6 +181,29 @@ CREATE TABLE `ExampleElementDataTable` (
 --
 -- Dumping routines for database 'ClayUI'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `uspGetApplicationDetails` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50020 DEFINER=`sqladmin`@`localhost`*/ /*!50003 PROCEDURE `uspGetApplicationDetails`(IN APPLICATION_ID INT)
+BEGIN
+    SELECT ApplicationID
+        ,ApplicationName
+        ,Description
+    FROM Applications
+    WHERE ApplicationID = APPLICATION_ID;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `uspGetAppMenuStructure` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -414,4 +437,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-03-19  4:02:02
+-- Dump completed on 2012-03-19 23:21:16
