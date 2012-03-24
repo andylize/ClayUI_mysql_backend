944af82066ac14ad0658ccdbe162c5e01c5bd6db
Andy Lize <alize@netinfocentral.com>
2012-03-24 18:04:10 -0500

    Added more routines.

--- ClayUI_mysql_backend/ClayUI.db.sql
+++ ClayUI_mysql_backend/ClayUI.db.sql
@@ -63,8 +63,8 @@ CREATE TABLE `Applications` (
   `ApplicationID` int(11) NOT NULL AUTO_INCREMENT,
   `ApplicationName` varchar(50) NOT NULL,
   `Description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ApplicationID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`ApplicationID`,`ApplicationName`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,6 +182,33 @@ CREATE TABLE `ExampleElementDataTable` (
 --
 -- Dumping routines for database 'ClayUI'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `uspAddApp` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50020 DEFINER=`sqladmin`@`localhost`*/ /*!50003 PROCEDURE `uspAddApp`()
+BEGIN
+    
+    SELECT @NEWID:= MAX(ApplicationID) + 1 FROM Applications;
+    
+    INSERT INTO Applications (ApplicationName, Description)
+    VALUES ('New_Application', 'New Application');
+
+    SELECT @NEWID:= LAST_INSERT_ID();
+
+    CALL uspGetApplicationDetails(@NEWID);
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `uspGetApplicationDetails` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -510,4 +537,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-03-20 21:39:39
+-- Dump completed on 2012-03-24 18:02:54
